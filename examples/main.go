5a225d42bfd57855dd99fd2cb794fb20173b9088
Michael Thornes <michael@zyte.dev>
2025-06-24 23:41:31 -0700

    update examples

--- examples/main.go
+++ examples/main.go
@@ -5,17 +5,16 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/zytekaron/squad"
+	"github.com/zytekaron/shamir-go"
 )
 
 func main() {
-	const n = 5
-	const k = 3
+	const k, n = 3, 5
 	secret := []byte("Hello World!")
 
 	// split your secret into n shares, where at least
 	// k of them are required to recover the secret
-	shares, err := Split(secret, n, k)
+	shares, err := shamir.Split(secret, n, k)
 	if err != nil {
 		log.Fatalln("error splitting secret:", err)
 	}
@@ -25,7 +24,7 @@ func main() {
 		fmt.Println(x, "=>", hex.EncodeToString(shares[x]))
 	}
 
-	// provide at least k(3) of the original n(5)
+	// provide at least k (3) of the original n (5)
 	// shares to successfully recover the secret
 	shares = map[byte][]byte{
 		1: shares[1],
@@ -34,6 +33,6 @@ func main() {
 	}
 
 	// combine the shares and you have your secret!
-	result := squad.Combine(shares)
+	result := shamir.Combine(shares)
 	fmt.Println(string(result)) // Hello, World!
 }
